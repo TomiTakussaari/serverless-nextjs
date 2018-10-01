a477c27a6ca0452e94d69ab7cb8925a75084ee31
Tomi Takussaari <tomi.takussaari@nitor.com>
2018-10-01 13:01:03 +0300

    Remove apigw binary

--- lambda.js
+++ lambda.js
@@ -4,30 +4,6 @@ process.env.NODE_ENV = "production";
 const serverless = require("serverless-http");
 const server = require("./server");
 
-const binaryMimeTypes = [
-    "application/javascript",
-    "application/json",
-    "application/octet-stream",
-    "application/xml",
-    "binary/octet-stream",
-    "image/jpeg",
-    "image/png",
-    "image/gif",
-    "text/comma-separated-values",
-    "text/css",
-    "text/html",
-    "text/javascript",
-    "text/plain",
-    "text/text",
-    "text/xml",
-    "image/x-icon",
-    "image/svg+xml",
-    "application/font-woff2",
-    "application/font-woff",
-    "font/woff",
-    "font/woff2"
-];
-
 let handler = null;
 
 module.exports.handler = (evt, ctx, callback) => {
@@ -35,9 +11,7 @@ module.exports.handler = (evt, ctx, callback) => {
     let initializerP;
     if (handler === null) {
         initializerP = prepareP.then(() => {
-            handler = serverless(appServer, {
-                binary: binaryMimeTypes
-            });
+            handler = serverless(appServer);
         });
     } else {
         initializerP = Promise.resolve();
